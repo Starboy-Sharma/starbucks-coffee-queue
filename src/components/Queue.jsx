68a24c11221375e9e38cd9ee3d412ed6e7a1716d
Pankaj Sharma <pankaj.sharma@emids.com>
2024-07-29 22:55:01 +0530

    add slideup animation when a order got resolved

--- src/components/Queue.jsx
+++ src/components/Queue.jsx
@@ -1,17 +1,22 @@
 // @ts-nocheck
 import { PropTypes } from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiCoffeeTogo } from 'react-icons/bi';
 
 // eslint-disable-next-line react/display-name
 const Queue = React.memo(({ isFirstPerson, item, windowId, dequeueOrder }) => {
   console.log('Set time out runing for ', item.id, windowId);
+  const [isItemRemoved, setIsItemRemoved] = useState(false);
 
   useEffect(() => {
     if (isFirstPerson) {
       const timer = setTimeout(() => {
         console.log('Remove item ', item.id, item.value, windowId);
-        dequeueOrder(windowId, item.id);
+        setIsItemRemoved(true);
+        // delay the fn call to show the slideUp animation after the item is removed
+        setTimeout(() => {
+          dequeueOrder(windowId, item.id);
+        }, 400);
       }, 10000);
 
       return () => clearTimeout(timer); // Clean up the timer on component unmount
@@ -19,7 +24,7 @@ const Queue = React.memo(({ isFirstPerson, item, windowId, dequeueOrder }) => {
   }, [isFirstPerson]);
 
   return (
-    <div className="Queue">
+    <div className={`Queue ${isItemRemoved ? 'list-item-exit' : ''}`}>
       <p>
         <BiCoffeeTogo size="16px" color="#362415" /> {item.value} coffee
       </p>
